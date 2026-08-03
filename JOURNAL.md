924b6cd1044511c3cf180d670854114b233034d4
Nico Smith <web@niiccoo2.xyz>
2026-08-03 16:08:28 -0400

    WIP: Add subscriptions

--- JOURNAL.md
+++ JOURNAL.md
@@ -1,12 +1,12 @@
 ### Tuesday, July 28th | 1 hour
 
-#### 21:20 | x hour
+#### 21:20 | 1 hour
 
 Going to see how fast I can get the MVP of this made. For now I just want it to save everything locally, although in the near future I want it to save to a server and support accounts. Would be great if I could just do like code based log in.
 
 Using inline css for now but I should clean it up at some point.
 
-### Sunday, August 2nd | x hours
+### Sunday, August 2nd | 2 hours
 
 #### 18:15 | 1.5 hours
 
@@ -14,6 +14,12 @@ Switching the UI tocarbon design and going to make it display a list of transact
 
 For now, this is just going to basically take how ever much money I make, devide by two, and tell me to send it to savings. It will also look at my subscriptions and tell me to pay myself back for them. It is not going to track what is happening in my savings yet. Because that gets complicated with stocks and all that. Might add all that later. Again, this is really just an MVP, so I can add more later.
 
-#### 21:40 | x hours
+#### 21:40 | .5 hours
 
 Now going to work on the part that tells me how much money I need to transfer to my savings account. There are a lot of better ways to do this (code wise), but they take more time, and right now, I don't really have time, I just want a working product.
+
+### Monday, August 3rd | x hours
+
+#### 15:00 | x hours
+
+Going to finish the send to savings UI and add subscriptions.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,35 +9,55 @@
 		TextInput,
 		Form,
 		OverflowMenu,
-		OverflowMenuItem
+		OverflowMenuItem,
+		Checkbox,
+		Tag,
+		Tabs,
+		Tab
 	} from 'carbon-components-svelte';
-
+	import type { Writable } from 'svelte/store';
 	import { createLocalStorageStore } from '$lib/localStorage';
 
-	let openTransactionDialog = false;
+	let openTransactionDialog: boolean = false;
+	let newItemName: string = '';
+	let newItemAmount: number = 0;
+	let savingsCheck: boolean = false;
+	let tabIndex: number = 0;
 
 	type Item = {
 		id: number;
 		date: string;
 		item: string;
+		type: string;
 		amount: number;
 	};
 
-	const itemsStore = createLocalStorageStore<Item[]>('app_items', []);
+	type Subscription = {
+		id: number;
+		service: string;
+		cost: number;
+		period: number;
+		startDate: Date;
+		endDate: Date;
+	};
 
-	function addItem(name: string, amount: number) {
+	const itemsStore = createLocalStorageStore<Item[]>('items', []);
+	const subscriptionsStore = createLocalStorageStore<Subscription[]>('subscriptions', []);
+
+	function addItem(name: string, type: string, amount: number) {
 		const newItem: Item = {
 			id: Date.now(),
 			date: new Date().toLocaleDateString(),
 			item: name,
+			type: type,
 			amount: amount
 		};
 
 		$itemsStore = [...$itemsStore, newItem];
 	}
 
-	function removeItem(id: number) {
-		$itemsStore = $itemsStore.filter((e) => e.id != id);
+	function removeItem<T extends { id: number }>(store: Writable<T[]>, id: number) {
+		store.update((items) => items.filter((item) => item.id !== id));
 	}
 
 	function makeNiceNumber(number: number) {
@@ -45,66 +65,166 @@
 		else return `-$${number.toString().slice(1)}`;
 	}
 
-	let newItemName: string = '';
-	let newItemAmount: number = 0;
+	function calculateAmountSendSavings() {
+		let sendSavingsAmount: number = 0;
 
-	function handleSubmit(event: SubmitEvent) {
-		event.preventDefault();
+		for (let item of $itemsStore) {
+			if (item.type == 'normal' && item.amount > 0) sendSavingsAmount += item.amount / 2;
+			else if (item.type == 'savings' && item.amount < 0) sendSavingsAmount += item.amount;
+		}
 
-		addItem(newItemName, newItemAmount);
-
-		newItemName = '';
-		newItemAmount = 0;
-		openTransactionDialog = false;
+		return sendSavingsAmount;
 	}
 </script>
 
 <div class="main-element">
-	<h1>Budget tracker</h1>
-	<DataTable
-		headers={[
-			{ key: 'date', value: 'Date', width: '15%' },
-			{ key: 'item', value: 'Item', width: '70%' },
-			{ key: 'amount', value: 'Amount', width: '15%' },
-			{ key: 'overflow', empty: true, width: '72px' }
-		]}
-		rows={$itemsStore}
-	>
-		<svelte:fragment slot="cell" let:cell let:row>
-			{#if cell.key === 'overflow'}
-				<OverflowMenu portalMenu flipped>
-					<OverflowMenuItem on:click={() => removeItem(row.id)} danger text="Remove" />
-				</OverflowMenu>
-			{:else if cell.key === 'amount'}
-				<span class:positive={cell.value > 0}>
-					{makeNiceNumber(cell.value)}
-				</span>
-			{:else}
-				{cell.value}
-			{/if}
-		</svelte:fragment>
-		<Toolbar>
-			<ToolbarContent>
-				<Button on:click={() => (openTransactionDialog = true)}>Add transaction</Button>
-			</ToolbarContent>
-		</Toolbar>
-	</DataTable>
-</div>
+	<div class="header">
+		<h1 class="first">Budget tracker</h1>
 
-<Modal bind:open={openTransactionDialog} modalHeading="Add transaction" passiveModal>
-	<Form on:submit={handleSubmit}>
-		<TextInput bind:value={newItemName} fluid labelText="Item" />
-		<NumberInput bind:value={newItemAmount} fluid hideSteppers labelText="Amount" />
-
-		<!-- Custom button container for normal-sized side-by-side buttons -->
-		<div class="button-row">
-			<Button kind="secondary" size="field" on:click={() => (openTransactionDialog = false)}>
-				Cancel
-			</Button>
-			<Button type="submit" size="field">Submit</Button>
+		<div>
+			<Tabs bind:selected={tabIndex}>
+				<Tab label="Transactions" />
+				<Tab label="Subscriptions" />
+			</Tabs>
 		</div>
-	</Form>
-</Modal>
+	</div>
+
+	{#if tabIndex == 0}
+		<div>
+			{#if calculateAmountSendSavings() > 0}
+				<Tag type="red">You must send ${calculateAmountSendSavings()} to savings.</Tag>
+			{:else}
+				<Tag type="red" style="visibility: hidden;"
+					>You must send ${calculateAmountSendSavings()} to savings.</Tag
+				>
+			{/if}
+		</div>
+		<DataTable
+			headers={[
+				{ key: 'date', value: 'Date', width: '15%' },
+				{ key: 'item', value: 'Item', width: '70%' },
+				{ key: 'amount', value: 'Amount', width: '15%' },
+				{ key: 'overflow', empty: true, width: '72px' }
+			]}
+			rows={$itemsStore}
+		>
+			<svelte:fragment slot="cell" let:cell let:row>
+				{#if cell.key === 'overflow'}
+					<OverflowMenu portalMenu flipped>
+						<OverflowMenuItem
+							on:click={() => removeItem(itemsStore, row.id)}
+							danger
+							text="Remove"
+						/>
+					</OverflowMenu>
+				{:else if cell.key === 'amount'}
+					<span class:positive={cell.value > 0}>
+						{makeNiceNumber(cell.value)}
+					</span>
+				{:else}
+					{cell.value}
+				{/if}
+			</svelte:fragment>
+			<Toolbar>
+				<ToolbarContent>
+					<Button on:click={() => (openTransactionDialog = true)}>Add transaction</Button>
+				</ToolbarContent>
+			</Toolbar>
+		</DataTable>
+
+		<Modal bind:open={openTransactionDialog} modalHeading="Add transaction" passiveModal>
+			<Form
+				on:submit={(e) => {
+					e.preventDefault();
+
+					if (savingsCheck) {
+						const newItem: Item = {
+							id: Date.now(),
+							date: new Date().toLocaleDateString(),
+							item: 'Savings transfer',
+							type: 'savings',
+							amount: newItemAmount
+						};
+					} else {
+						const newItem: Item = {
+							id: Date.now(),
+							date: new Date().toLocaleDateString(),
+							item: newItemName,
+							type: 'normal',
+							amount: newItemAmount
+						};
+					}
+
+					$itemsStore = [...$itemsStore, newItem];
+
+					newItemName = '';
+					newItemAmount = 0;
+					savingsCheck = false;
+					openTransactionDialog = false;
+				}}
+			>
+				<Checkbox
+					bind:checked={savingsCheck}
+					style="margin: 0 0 2vh 0"
+					labelText="Savings transfer"
+				/>
+				{#if !savingsCheck}
+					<TextInput bind:value={newItemName} fluid labelText="Item" />
+				{/if}
+				<NumberInput bind:value={newItemAmount} fluid hideSteppers labelText="Amount" />
+
+				<div class="button-row">
+					<Button kind="secondary" size="field" on:click={() => (openTransactionDialog = false)}>
+						Cancel
+					</Button>
+					<Button type="submit" size="field">Submit</Button>
+				</div>
+			</Form>
+		</Modal>
+	{:else}
+		<div>
+			<Tag type="red" style="visibility: hidden;">I need a tag here for spacing and I'm lazy.</Tag>
+		</div>
+		<DataTable
+			headers={[
+				{ key: 'service', value: 'Service', width: '70%' },
+				{ key: 'cost', value: 'Cost', width: '15%' },
+				{ key: 'period', value: 'Period', width: '15%' },
+				{ key: 'startDate', value: 'Start', width: '15%' },
+				{ key: 'endDate', value: 'End', width: '15%' },
+				{ key: 'overflow', empty: true, width: '72px' }
+			]}
+			rows={$subscriptionsStore}
+		>
+			<svelte:fragment slot="cell" let:cell let:row>
+				{#if cell.key === 'overflow'}
+					<OverflowMenu portalMenu flipped>
+						<OverflowMenuItem
+							on:click={() => removeItem(subscriptionsStore, row.id)}
+							danger
+							text="Remove"
+						/>
+					</OverflowMenu>
+				{:else if cell.key === 'cost'}
+					{makeNiceNumber(cell.value)}
+				{:else if cell.key === 'period'}
+					{#if cell.value > 1}
+						{cell.value} months
+					{:else}
+						{cell.value} month
+					{/if}
+				{:else}
+					{cell.value}
+				{/if}
+			</svelte:fragment>
+			<Toolbar>
+				<ToolbarContent>
+					<Button on:click={() => (openTransactionDialog = true)}>Add subscription</Button>
+				</ToolbarContent>
+			</Toolbar>
+		</DataTable>
+	{/if}
+</div>
 
 <style>
 	.positive {
@@ -124,5 +244,14 @@
 		justify-content: flex-end;
 		gap: 1rem;
 		margin-top: 1.5rem;
+	}
+
+	.header {
+		display: flex;
+		align-items: center;
+	}
+
+	.first {
+		flex: 1;
 	}
 </style>
